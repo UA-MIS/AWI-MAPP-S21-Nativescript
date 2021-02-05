c95069e4a2c9b6f8299e260e3177e3393c11670f
Annie Meyer <akmeyerrasmussen@crimson.ua.edu>
2021-02-05 14:00:34 -0600

    created location module

---
+++ src/app/shared/location/location.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'Location',
+    templateUrl: './location.component.html'
+  })
+  //implements OnInit 
+  export class LocationComponent {
+    constructor() { }
+  
+    ngOnInit() { }
+  }
+  
