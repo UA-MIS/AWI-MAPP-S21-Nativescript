cb41143215eb1db7b9ea374ca6efb4e925348f86
ryannalbrechtt <rtalbrecht@crimson.ua.edu>
2021-02-08 12:18:11 -0600

    1st commit

---
+++ src/app/shared/tide.service.ts
@@ -0,0 +1,22 @@
+/*
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TideService{
+	constructor(private http: HttpClient){}
+
+	getRawTideData(stationNum: number): Observable<any>{
+		'Content-Type: 'application/json'
+	});
+	return this.http.get(
+
+
+}
+
+*/
